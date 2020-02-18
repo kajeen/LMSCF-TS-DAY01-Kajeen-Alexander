7e3160f5f2010b0e1576afcc8e1847ebd0cc346e
Kajeen Ahmed <kajeenahmed2@icloud.com>
2020-02-18 11:48:36 +0100

    irgend

--- Kajeen/Restaurant.ts
+++ Kajeen/Restaurant.ts
@@ -1,11 +1,15 @@
 let nameOfDish: Array<string> =["apple", "bread", "cheese","fish","ham","orange"] ;
-// for (var value of nameOfDish) {
-// console.log(value);
-// }
-const [a,b,c,d,e,f] = nameOfDish;
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(f);
+
+for (var value of nameOfDish) {
+	console.log(value);
+	$("ul").append(`<li><a href="${value}"><p>${value}</p></a></li>`);
+};
+
+
+// const [a,b,c,d,e,f] = nameOfDish;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
--- Kajeen/restaurant.js
+++ Kajeen/restaurant.js
@@ -1,11 +1,14 @@
 var nameOfDish = ["apple", "bread", "cheese", "fish", "ham", "orange"];
-// for (var value of nameOfDish) {
-// console.log(value);
-// }
-var a = nameOfDish[0], b = nameOfDish[1], c = nameOfDish[2], d = nameOfDish[3], e = nameOfDish[4], f = nameOfDish[5];
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(f);
+for (var _i = 0, nameOfDish_1 = nameOfDish; _i < nameOfDish_1.length; _i++) {
+    var value = nameOfDish_1[_i];
+    console.log(value);
+    $("ul").append("<li><a href=\"" + value + "\"><p>" + value + "</p></a></li>");
+}
+;
+// const [a,b,c,d,e,f] = nameOfDish;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
