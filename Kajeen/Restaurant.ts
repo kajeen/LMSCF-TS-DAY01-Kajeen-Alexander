139dff85f3006403c7473a82837e53a311de11b5
Kajeen Ahmed <kajeenahmed2@icloud.com>
2020-02-18 11:14:09 +0100

    advance

---
+++ Kajeen/Restaurant.ts
@@ -0,0 +1,11 @@
+let nameOfDish: Array<string> =["apple", "bread", "cheese","fish","ham","orange"] ;
+// for (var value of nameOfDish) {
+// console.log(value);
+// }
+const [a,b,c,d,e,f] = nameOfDish;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
---
+++ Kajeen/restaurant.js
@@ -0,0 +1,11 @@
+var nameOfDish = ["apple", "bread", "cheese", "fish", "ham", "orange"];
+// for (var value of nameOfDish) {
+// console.log(value);
+// }
+var a = nameOfDish[0], b = nameOfDish[1], c = nameOfDish[2], d = nameOfDish[3], e = nameOfDish[4], f = nameOfDish[5];
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
